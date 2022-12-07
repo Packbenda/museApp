5c30c2b031d426afb913ee6ca8a45c0b264b9a6f
Packbenda <73231621+Packbenda@users.noreply.github.com>
2022-12-07 10:59:16 +0100

    supprimer tout

--- Mus/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class MusConfig(AppConfig):
-    name = 'Mus'
-
--- Mus/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('vote/<str:pk>', views.vote, name = 'vote'),
-    path('result/<str:pk>', views.result, name = 'result'),
-    path('spotify/<str:pk>', views.spotify, name = 'spotify',)
-]
